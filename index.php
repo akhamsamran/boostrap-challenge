9658bb1156416139a9daf57e51cf9c49e470db3c
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-02 21:14:38 -0700

    put my logo in the nav bar

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 			<!--this is the navigation bar that has to have a little dropdown menu icon when small, but horizontal menu words when big-->
 		<div class="container-flex">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light border rounded">
-				<a class="navbar-brand" href="#">Anna Khamsamran</a>
+				<a class="navbar-brand" href=""><img src="images/aklogosm.png" alt="logo"/></a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
