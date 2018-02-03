2dbb66ced23b69c34e218e2eb4ea07c2a2ab4790
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-02 17:28:16 -0700

    started head setup for bootstrap index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!--meta tags and stuff-->
+		<meta charset="UTF-8" />
+
+		<!--BOOTSTRAP CSS-->
+
+		<!--Other and custom CSS-->
+
+		<!--Optional Javascript links-->
+		<!--link to Bootstrap javascript dependencies-->
+		<!--(if it is a heavy site, put this block just below the closing body tag)-->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+		<title>Anna's first Bootstrap website</title>
+	</head>
+	<body>
+		<h1></h1>
+	</body>
+</html>
