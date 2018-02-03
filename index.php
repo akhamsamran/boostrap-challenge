7a13b9bc66c180db8a5e668d903735d2c2c4ae8b
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-02 18:07:41 -0700

    added navbar from bootstrap documentation website and added a border and rounded corners to the standard setup

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="en">
 
 	<head>
@@ -26,10 +27,35 @@
 
 	</head>
 	<body>
-		<div class="container">
+		<header>
+		<div class="container-fluid">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light border rounded">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div class="navbar-nav">
+						<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+						<a class="nav-item nav-link" href="#">Features</a>
+						<a class="nav-item nav-link" href="#">Pricing</a>
+						<a class="nav-item nav-link disabled" href="#">Disabled</a>
+					</div>
+				</div>
+			</nav>
 
-
-
+			<span class="border" class="rounded"></span>
 		</div>
+			<main>
+				<div class="container-fluid">
+				</div>
+			</main>
+		</header>
+		<footer>
+			<div class="container-fluid">
+
+			</div>
+
+		</footer>
 	</body>
 </html>
