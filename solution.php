89f56f49d763a95e24a58581bcde9bc98be3ec5e
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-08 15:44:13 -0700

    process on solution

---
+++ solution.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+
+		<!--meta tags and stuff-->
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+		<!--BOOTSTRAP CSS-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<!--Other and custom CSS-->
+
+		<!--Optional Javascript links-->
+		<!--link to Bootstrap javascript dependencies-->
+		<!--(Put it here, but if it is a heavy site, put this block just below the closing body tag)-->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+		<title>Anna's trial Bootstrap website</title>
+
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#">NAKHA</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav ml-auto">
+							<li class="nav-item">
+								<a class="nav-link" href="#">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link 2</a>
+							</li>
+
+							<li class="nav-item">
+								<a class="nav-link " href="#">link 3</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link " href="#">link 4</a>
+							</li>
+						</ul>
+
+						</form>
+					</div>
+				</nav>
+
+			</div>
+		</header>
+
+			<section>
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div class="jumbotron">
+								<h1 class="display-4">Hello, world!</h1>
+								<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+								<hr class="my-4">
+								<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+								<p class="lead">
+									<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section>
+
+			</section>
+			<section>
+
+			</section>
+
+		<footer>
+
+		</footer>
+	</body>
+</html>
